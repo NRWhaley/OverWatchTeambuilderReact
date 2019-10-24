23ddd886168e410365c1da8a377e40dff33bf9e2
Nick Whaley <nicholasrwhaley@gmail.com>
2019-10-24 11:58:29 -0700

    add character and refactor to separate character types

---
+++ owteambuilder/src/components/Heroes.js
@@ -0,0 +1,50 @@
+
+const heroes = [
+  dps : [
+    {name: 'Ashe', primary: 160, health: 200 , ultCost: 2240, pic: undefined},
+    {name: 'Bastion', primary: 104, health: 300, ultCost: 2310, pic: undefined},
+    {name: 'Doomfist', primary: 100, health: 200, ultCost: 2000, pic: undefined},
+    {name: 'Genji', primary: 37.33, health: 200, ultCost: 1680, pic: undefined },
+    {name: 'Hanzo', primary: 77 , health: 200 , ultCost: 1680, pic: undefined},
+    {name: 'Junkrat', primary: 76.78 , health: 200 , ultCost: 1925 , pic: undefined},
+    {name: 'McCree', primary: 90 , health: 200 , ultCost: 1680, pic: undefined},
+    {name: 'Mei', primary: 55, health: 250, ultCost: 1610, pic: undefined},
+    {name: 'Pharah', primary: 90.23 , health: 200, ultCost: 2100 , pic: undefined},
+    {name: 'Reaper', primary: 182, health: 250, ultCost: 2100 , pic: undefined},
+    {name: 'Soldier76', primary: 129.9, health: 200, ultCost: 2310, pic: undefined},
+    {name: 'Torbjorn', primary: 116.9 , health: 200, ultCost: 2380, pic: undefined},
+    {name: 'Tracer', primary: 156, health: 150, ultCost: 1260 , pic: undefined},
+    {name: 'Sombra', primary: 104 , health: 200, ultCost: 1400, pic: undefined},
+    {name: 'Widowmaker', primary: 130, health: 200, ultCost: 1540, pic: undefined},
+    {name: 'Symmetra', primary: 130, health: 200, ultCost: 1680, pic: undefined}
+
+  ],
+  tank: [
+
+  {name: 'Dva', primary: 95.38, health: 750, ultCost: 1540, pic: undefined},
+  {name: 'Orisa', primary: 137.5 , health: 400, ultCost: 1680 , pic: undefined},
+  {name: 'Reinhardt', primary: 82.5, health: 500, ultCost: 1540, pic: undefined},
+  {name: 'Roadhog', primary: 126.75 , health: 600 , ultCost: 2240, pic: undefined},
+  {name: 'Sigma', primary: 100 , health: 400 , ultCost: 2170 , pic: undefined},
+  {name: 'WreckingBall', primary: 125, health: 600, ultCost: 1540 , pic: undefined},
+  {name: 'Zarya', primary: 142.5, health: 400, ultCost: 2100 , pic: undefined},
+  {name: 'Winston', primary: 120, health: 500, ultCost: 1540, pic: undefined}
+
+  ],
+  support: [
+    {name: 'Ana', primary: 87.5, health: 200, ultCost: 2100, pic: undefined },
+    {name: 'Baptiste', primary: 168.75, health: 200, ultCost: 1960, pic: undefined},
+    {name: 'Brigitte', primary: 100, health: 200, ultCost: 1500, pic: undefined},
+    {name: 'Lucio', primary: 38.46 , health: 200 , ultCost: 2940 , pic: undefined},
+    {name: 'Mercy', primary: 100, health: 200, ultCost: 1820, pic: undefined},
+    {name: 'Moira', primary: 50 , health: 200 , ultCost: 2380 , pic: undefined},
+    {name: 'Zenyatta', primary: 120, health: 200 , ultCost: 2310, pic: undefined},
+  ]
+
+]
+
+
+
+
+
+export default heroes;
---
+++ owteambuilder/src/components/heroBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+class heroBox extends React.Component {
+
+
+
+
+
+}
